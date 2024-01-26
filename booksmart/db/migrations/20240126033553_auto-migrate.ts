7b3de547f783bdcf43df7f36a63cedc5bca83e91
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-26 14:19:53 +0800

    patch: fix foreign key error when migrate

--- booksmart/db/migrations/20240126033553_auto-migrate.ts
+++ booksmart/db/migrations/20240126033553_auto-migrate.ts
@@ -1,6 +1,7 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.raw('delete from `service_option`')
   await knex.raw('delete from `service`')
   await knex.raw('alter table `service` add column `slug` text not null')
 }
