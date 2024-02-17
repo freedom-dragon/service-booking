72e54fc43bfcf20fb38b660b7d7da755e3d94646
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-17 14:08:13 +0800

    db: fix seed

--- booksmart/db/migrations/20240216195433_auto-migrate.ts
+++ booksmart/db/migrations/20240216195433_auto-migrate.ts
@@ -3,6 +3,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.raw('delete from `service_option`')
+  await knex.raw('delete from `service_timeslot`')
   await knex.raw('delete from `service`')
   await knex.raw('alter table `service` add column `unit_price` integer not null')
 }
