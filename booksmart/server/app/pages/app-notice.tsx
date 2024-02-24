968ccff8e90d75a4ce83301d781b7caf914fa851
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-25 04:35:30 +0800

    ui: add sample data to notice page

--- booksmart/server/app/pages/app-notice.tsx
+++ booksmart/server/app/pages/app-notice.tsx
@@ -7,7 +7,7 @@ import { mapArray } from '../components/fragment.js'
 import { appIonTabBar } from '../components/app-tab-bar.js'
 import { fitIonFooter, selectIonTab } from '../styles/mobile-style.js'
 
-let pageTitle = 'Notice'
+let pageTitle = '通知'
 
 let style = Style(/* css */ `
 #Notice {
@@ -26,7 +26,6 @@ let page = (
       </ion-toolbar>
     </ion-header>
     <ion-content id="Notice" class="ion-padding">
-      Items
       <Main />
     </ion-content>
     <ion-footer>
@@ -38,8 +37,8 @@ let page = (
 )
 
 let items = [
-  { title: 'Android', slug: 'md' },
-  { title: 'iOS', slug: 'ios' },
+  { title: 'Katy 已確定了情侶班的預訂', timestamp: '2024-02-25 13:30' },
+  { title: 'Katy 已確定了體驗班的預訂', timestamp: '2024-02-20 11:15' },
 ]
 
 function Main(attrs: {}, context: Context) {
@@ -48,7 +47,10 @@ function Main(attrs: {}, context: Context) {
       <ion-list>
         {mapArray(items, item => (
           <ion-item>
-            {item.title} ({item.slug})
+            <ion-label>{item.title}</ion-label>
+            <ion-note slot="end" color="dark" style="font-size: smaller">
+              ({item.timestamp})
+            </ion-note>
           </ion-item>
         ))}
       </ion-list>
