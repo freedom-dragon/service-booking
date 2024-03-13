b6bfc6a8f40e9218314afab39a1def074cbcb48a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-03-14 06:36:29 +0800

    feat: implemented notice page for shop owner

--- booksmart/server/app/pages/app-notice.tsx
+++ booksmart/server/app/pages/app-notice.tsx
@@ -6,6 +6,12 @@ import { Context } from '../context.js'
 import { mapArray } from '../components/fragment.js'
 import { appIonTabBar } from '../components/app-tab-bar.js'
 import { fitIonFooter, selectIonTab } from '../styles/mobile-style.js'
+import { getAuthUser } from '../auth/user.js'
+import { filter, find } from 'better-sqlite3-proxy'
+import { proxy } from '../../../db/proxy.js'
+import { db } from '../../../db/db.js'
+import DateTimeText, { formatDateTimeText } from '../components/datetime.js'
+import { WEEK } from '@beenotung/tslib/time.js'
 
 let pageTitle = '通知'
 
@@ -36,12 +42,56 @@ let page = (
   </>
 )
 
-let items = [
-  { title: 'Katy 已確定了情侶班的預約', timestamp: '2024-02-25 13:30' },
-  { title: 'Katy 已確定了體驗班的預約', timestamp: '2024-02-20 11:15' },
-]
+let select_booking_by_shop = db
+  .prepare(
+    /* sql */ `
+select
+  booking.id
+from booking
+inner join service on service.id = booking.service_id
+where service.shop_id = :shop_id
+order by booking.updated_at desc
+`,
+  )
+  .pluck()
 
 function Main(attrs: {}, context: Context) {
+  let user = getAuthUser(context)
+  let shop = user ? find(proxy.shop, { owner_id: user.id! }) : null
+  let items: { title: string; timestamp: string }[] = [
+    // { title: 'Katy 已確定了情侶班的預約', timestamp: '2024-02-25 13:30' },
+    // { title: 'Katy 已確定了體驗班的預約', timestamp: '2024-02-20 11:15' },
+  ]
+  if (shop) {
+    let booking_ids = select_booking_by_shop.all({
+      shop_id: shop.id,
+    }) as number[]
+    for (let id of booking_ids) {
+      let booking = proxy.booking[id]
+      if (booking.cancel_time) {
+        items.push({
+          title: (
+            <span>
+              {booking.user!.nickname} 取消了 {booking.service!.name} 的預約
+            </span>
+          ),
+          timestamp: timestamp(booking.cancel_time),
+        })
+      } else {
+        items.push({
+          title: (
+            <span>
+              {booking.user!.nickname} 申請預約 {booking.service!.name}
+            </span>
+          ),
+          timestamp: timestamp(booking.submit_time),
+        })
+      }
+    }
+  }
+  if (items.length == 0) {
+    return <p class="ion-text-center">暫時未有通知</p>
+  }
   return (
     <>
       <ion-list>
@@ -55,6 +105,14 @@ function Main(attrs: {}, context: Context) {
         ))}
       </ion-list>
     </>
+  )
+}
+
+function timestamp(time: number) {
+  return (
+    <time datetime={new Date(time).toISOString()}>
+      <DateTimeText time={time} relativeTimeThreshold={2 * WEEK} />
+    </time>
   )
 }
 
