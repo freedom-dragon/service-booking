d5fe4f434ebd692c2d60290c9cf10629072847a6
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-04-02 07:11:23 +0800

    patch: do not count canceled booking

--- booksmart/server/app/booking-store.ts
+++ booksmart/server/app/booking-store.ts
@@ -1,22 +1,21 @@
-import { Service, User, proxy } from '../../db/proxy.js'
-import { count } from 'better-sqlite3-proxy'
+import { Booking, Service, User, proxy } from '../../db/proxy.js'
+import { count, notNull } from 'better-sqlite3-proxy'
 
 export function countBooking(options: {
   service: Service
   user?: User | null
 }) {
   let times = options.service.times || 1
-  let total_used = count(
-    proxy.booking,
-    options.user
-      ? {
-          service_id: options.service.id!,
-          user_id: options.user.id!,
-        }
-      : {
-          service_id: options.service.id!,
-        },
-  )
+  let filter: Partial<Booking> = {
+    service_id: options.service.id!,
+    approve_time: notNull,
+    cancel_time: null,
+    reject_time: null,
+  }
+  if (options.user) {
+    filter.user_id = options.user.id!
+  }
+  let total_used = count(proxy.booking, filter)
   let used = total_used % times
   return { used, total_used, times }
 }
