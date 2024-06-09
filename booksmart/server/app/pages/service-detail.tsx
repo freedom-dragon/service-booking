47dcfa4f64d6da0eb2dd64f517f3eb11549a9cd0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-06-09 13:39:57 +0800

    ui: add margin to review button

--- booksmart/server/app/pages/service-detail.tsx
+++ booksmart/server/app/pages/service-detail.tsx
@@ -1211,6 +1211,7 @@ function copyUrl() {
                 fill="block"
                 color="primary"
                 size="normal"
+                class="ion-margin-horizontal"
                 href={
                   'https://www.google.com/maps/search/' +
                   encodeURIComponent(address)
