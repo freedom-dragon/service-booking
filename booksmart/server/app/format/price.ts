f80ff3d43137bfad209b001872b2713f5b21ebb0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-07-06 08:59:37 +0800

    ui: making it explicit that the booking is free

--- booksmart/server/app/format/price.ts
+++ booksmart/server/app/format/price.ts
@@ -1,6 +1,7 @@
 export function formatPrice(price: string | number | null): string | number {
   if (!price) return '-'
   if (price == '0' || +price!) price = +price!
+  if (price == 0) return '$0 (Free)'
   if (price) return '$' + price.toLocaleString()
   return price
 }
