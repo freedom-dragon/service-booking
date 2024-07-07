ebf48d6824d555e6871fc1259ab64626b7408c3d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-07-07 18:06:01 +0800

    chore: remove unsued code

--- booksmart/server/app/components/timestamp.tsx
+++ booksmart/server/app/components/timestamp.tsx
@@ -1,14 +1,7 @@
 import { format_relative_time, setLang } from '@beenotung/tslib/format.js'
 import { o } from '../jsx/jsx.js'
-import Style from './style.js'
 
 setLang('zh-HK')
-
-export let TimestampStyle = Style(/* css */ `
-.timestamp {
-
-}
-`)
 
 export function relative_timestamp(time: number) {
   return (
