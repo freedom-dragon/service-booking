14ce1cbd597b844a01c853c97e0b8a2d5ca47ebb
glorytsang <49528815+glorytsang@users.noreply.github.com>
2024-10-28 17:53:07 +0800

    wip: on-board-template swiper

--- booksmart/server/app/pages/on-board-template.tsx
+++ booksmart/server/app/pages/on-board-template.tsx
@@ -123,18 +123,41 @@ export function getTemplateImageLinks() {
 }
 
 function GenerateImage(attrs: {}, context: DynamicContext) {
-  let template = getTemplateImageLinks().template
-  console.log(template)
-  let elements: Node[] = []
-  (
-    for (let strings of template) {
-      element.push
-    }
-  )
+  let templates = getTemplateImageLinks().template
+  console.log('template: ', templates)
+  console.log('template: ' + templates)
+  if (!templates)
+    return 'error retrieving template images, please try again later.'
+  // {
+  //   templates.map(item => console.log(item))
+  // }
+  // let element = template.join(' ')
+  // console.log(element)
+  // (
+  //   for (let strings of template) {
+  //     element.push
+  //   }
+  // )
   return (
     <>
-    template.map()
-    {/* template.forEach
+      <swiper-container
+        class="mySwiper"
+        pagination="true"
+        pagination-clickable="true"
+        space-between="30"
+        slides-per-view="3"
+      >
+        {mapArray(templates, item => (
+          <swiper-slide>
+            <img src={item} />
+          </swiper-slide>
+        ))}
+      </swiper-container>
+    </>
+  )
+  // template.join
+  {
+    /* template.forEach
     {mapArray(attrs.images, (slide, i) => (
                 <span
                   class="swiper-pagination-image"
@@ -142,21 +165,24 @@ function GenerateImage(attrs: {}, context: DynamicContext) {
                 >
                   {slide}
                 </span>
-              ))} */}
-              {/* mapArray(attrs.images, (slide, i) => (
+              ))} */
+  }
+  {
+    /* mapArray(attrs.images, (slide, i) => (
                 <span
                   class="swiper-pagination-image"
                   onclick={`swiperSlide(this, '${i}')`}
                 >
                   {slide}
                 </span>
-              )) */}
-              template.map
+              )) */
+  }
+  {
+    /* template.map
       <SwiperSlide>
         <img src={template[i]} alt="template" />
-      </SwiperSlide>
-    </>
-  )
+      </SwiperSlide> */
+  }
 }
 
 function CreateAccount(attrs: {}, context: DynamicContext) {
@@ -322,15 +348,20 @@ function OnBoardTemplate(_attrs: {}, context: DynamicContext) {
           <div class="swiper-button-prev"></div>
         </div> */}
 
-        <swiper-container
+        {/* <swiper-container
           class="mySwiper"
           pagination="true"
           pagination-clickable="true"
           space-between="30"
           slides-per-view="3"
-        >
-          <GenerateImage />
-          <swiper-slide>Slide 1</swiper-slide>
+        > */}
+        {/* {getTemplateImageLinks().template.map(item => (
+        <SwiperSlide>
+          <img src={item} />
+        </SwiperSlide>
+      ))} */}
+        {<GenerateImage />}
+        {/* <swiper-slide>Slide 1</swiper-slide>
           <swiper-slide>Slide 2</swiper-slide>
           <swiper-slide>Slide 3</swiper-slide>
           <swiper-slide>Slide 4</swiper-slide>
@@ -338,8 +369,8 @@ function OnBoardTemplate(_attrs: {}, context: DynamicContext) {
           <swiper-slide>Slide 6</swiper-slide>
           <swiper-slide>Slide 7</swiper-slide>
           <swiper-slide>Slide 8</swiper-slide>
-          <swiper-slide>Slide 9</swiper-slide>
-        </swiper-container>
+          <swiper-slide>Slide 9</swiper-slide> */}
+        {/* </swiper-container> */}
 
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
       </form>
